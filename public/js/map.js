f221dcea6ed701612a008dc080fc9d2eadcefdf8
Suus ten Voorde <suus.ten.voorde@hva.nl>
2018-06-04 13:14:51 +0200

    circle draggable and centerpoint

--- public/js/map.js
+++ public/js/map.js
@@ -5,13 +5,14 @@
 	var map = {
 		mapboxAccessToken: 'pk.eyJ1IjoibWF4ZGV2cmllczk1IiwiYSI6ImNqZWZydWkyNjF3NXoyd28zcXFqdDJvbjEifQ.Dl3DvuFEqHVAxfajg0ESWg',
 		map: L.map('map', {
-			zoomControl: false
+			zoomControl: false,
+			dragging: false
 		}),
 		init: function () {
 			// Set the original view of the map:
 			this.map.setView([52.370216, 4.895168], 13);
 
-			L.tileLayer('https://api.mapbox.com/styles/v1/maxdevries95/cjesmtkaj8iqs2rmoe0bo17w7/tiles/256/{z}/{x}/{y}?access_token=' + this.mapboxAccessToken, {
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + this.mapboxAccessToken, {
 				maxZoom: 20,
 				id: 'mapbox.streets'
 			}).addTo(this.map);
@@ -55,20 +56,40 @@
 					'geometry': wellknown(geo)
 				};
 			});
-			this.createLayers(streets);
+			this.createCircle(streets);
 		},
-		createLayers: function(data) {
+		createCircle: function(data) {
 			var self = this;
 			var selectRadius = document.querySelector("#radius-selected");
+			var centerPoint = {lat: 52.370216, lng: 4.895168};
 
 			//create circle
-			var circle = L.circle([52.370216, 4.895168], {
+			var circle = L.circle([centerPoint.lat, centerPoint.lng], {
 					color: 'red',
 					fillColor: '#f03',
 					fillOpacity: 0.4,
 					clickable: false,
-					radius: 100
+					radius: 100,
+					zIndexOffset: 1000
 			}).addTo(self.map);
+
+			//draggable
+			circle.on({
+				 mousedown: function () {
+					 self.map.on('mousemove', function (e) {
+						 centerPoint.lat = e.latlng.lat
+						 centerPoint.lng = e.latlng.lng
+						 circle.setLatLng(e.latlng);
+					 });
+				 }
+			});
+			self.map.on('mouseup',function(e){
+				var userInput = circle.getRadius();
+				map.createStreets(data, circle, userInput);
+				self.map.removeEventListener('mousemove');
+			})
+
+			circle.bringToFront();
 
 			selectRadius.addEventListener("change", function(el){
 				changeRadius(el);
@@ -77,12 +98,12 @@
 			function changeRadius(el) {
 				var meters = el.target.value * 100;
 				circle.setRadius(meters);
-				map.creategeoJSON(data, circle, meters);
+				map.createStreets(data, circle, meters);
 			}
 
-			map.creategeoJSON(data, circle);
+			map.createStreets(data, circle);
 		},
-		creategeoJSON: function(data, circle, userInput) {
+		createStreets: function(data, circle, userInput) {
 			var circle_lat_long = circle.getLatLng();
 			var counter_points_in_circle = 0;
 			var meters_user_set = userInput;
@@ -94,20 +115,22 @@
 			};
 
 			//create geoJSON layer
-			L.geoJSON(data, {
+			var streets = L.geoJSON(data, {
 				onEachFeature: function(feature, layer) {
 					if(layer.feature.geometry.type !== "Point"){
 						var bounds = layer.getBounds();
-						var center = bounds.getCenter()
+						var center = bounds.getCenter();
 						var distance_from_layer_circle = center.distanceTo(circle_lat_long);
 
 						if (distance_from_layer_circle <= meters_user_set) {
+							// console.log('straatnaam: ' + feature.properties.streetName);
 							counter_points_in_circle += 1;
 						}
 					}
 				},
 				style: function (feature) {
 					return {
+						zIndexOffset: 100,
 						weight: 1,
 						lineCap: 'square',
 						lineJoin: 'square',
@@ -116,41 +139,34 @@
 				},
 				pointToLayer: function (feature, latlng) { return L.circleMarker(latlng, geojsonMarkerOptions); }
 			})
-			.addTo(this.map)
+			// .addTo(this.map)
 			.on('mouseover', this.handleHoverOverStreet)
 			.on('click', function (e) {
 				events.handleClickOnStreet(e.layer.feature);
 			});
+
+			//Bring to back
+			streets.bringToBack();
+			console.log(circle.getLatLng(), userInput);
+
+
+			//Count number of streets
 			var number_of_streets = document.querySelector('.count-streets');
 			number_of_streets.innerHTML = counter_points_in_circle + " straten";
 		},
 		handleHoverOverStreet: function (e) {
-			var point = L.point(0, -5);
-
-			L.popup({
-				closeButton: false,
-				offset: point
-			})
-				.setLatLng(e.latlng)
-				.setContent(e.layer.feature.properties.streetName)
-				.openOn(map.map);
+			// var point = L.point(0, -5);
+			//
+			// L.popup({
+			// 	closeButton: false,
+			// 	offset: point
+			// })
+			// 	.setLatLng(e.latlng)
+			// 	.setContent(e.layer.feature.properties.streetName)
+			// 	.openOn(map.map);
 		}
 	};
 
-  /* mapbox */
-	// var map = {
-	// 	init: function() {
-	// 		this.create();
-	// 	},
-	// 	create: function() {
-	// 		mapboxgl.accessToken = 'pk.eyJ1Ijoic3V1c3RlbnZvb3JkZSIsImEiOiJjamVmc3Q3MDQxbGJ0MzNrdHE4Y3QwMW82In0.AWAlzy0cXMbrfR8Ed-7DOg'
-	// 		    var map = new mapboxgl.Map({
-	// 		    container: 'map',
-	// 		    style: 'mapbox://styles/mapbox/light-v9',
-	// 		    center: [4.8776926, 52.3563219],
-	// 		    zoom: '11'
-	// 		})
-	// 	}
-	// }
+
 	map.init()
 })()
