fad8b8c08521e23f47f36b3ac39c21f6c8a7c911
Max de Vries <max.devries.1995@gmail.com>
2018-06-05 09:58:17 +0200

    Change detail to story

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,22 +1,19 @@
 var fetch = require('node-fetch');
 var sparqlFile = require('./sparql');
-var rows = [];
 
 exports.homePage = function (req, res, next) {
   res.render('index');
 }
 
-exports.detailPage = function (req, res, next) {
-  var id = req.params.id
+exports.storyPage = function (req, res, next) {
 	var host = sparqlFile.data;
 
 	if(Object.keys(rows).length === 0) {
 		fetch(host)
 		.then((resp) => resp.json()) // transform the data into json
 			.then(function(data) {
-				res.render('detail', {
-					posters: data,
-					id: id
+				res.render('story', {
+					streets: data
 				});
 			}).catch(function(error) {
 				// if there is any error you will catch them here
@@ -24,9 +21,8 @@ exports.detailPage = function (req, res, next) {
 			});
 
 	} else {
-	 res.render('detail', {
-		 posters: rows,
-		 id: id
+	 res.render('story', {
+		 streets: []
 	 });
 	}
 }
