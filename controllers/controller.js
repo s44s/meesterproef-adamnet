2449734373ed3eef67ac642d852bdb69abbe3620
Max de Vries <max.devries.1995@gmail.com>
2018-06-07 19:04:48 +0200

    Add filter setup explanation

--- controllers/controller.js
+++ controllers/controller.js
@@ -65,8 +65,41 @@ exports.getCreateStoryPage = function (req, res, next) {
     fetch(url)
 		.then((resp) => resp.json()) // transform the data into json
       .then(function (data) {
+        console.log(data.results.bindings);
+
+        // First data filter:
+
+        /*
+        * Order the streets by distance to centerPoint (closest first)
+        * This gives us the order of the chapters based on location.
+        *
+        * Chapters based on location:
+        * ---------------------------
+        * 1. Closest street to centerPoint: probably your old/current street
+        * 2. Streets within a range of 25% from centerPoint: probably your neighbourhood
+        * 3. Other streets surrounding your neighbourhood
+        */
+
+        /*
+        * First chapter:
+        * --------------
+        * Filter the data for every image that has the following timestamp and location:
+        *
+        * Timestamp:
+        * ----------
+        * If timestamp <= 5: filter per year
+        * If timestamp > 5 & <= 20: filter per 2 years
+        * If timestamp > 20: filter per 5 years
+        *
+        * Location:
+        * ---------
+        * Take closest street to centerPoint as main location
+        */
+
         res.render('create-story', {
-          dataFirstQuery: data
+          // dataFirstQuery: data
+          chapter1: data.chapter1,
+          chapter2: data.chapter2,
         });
       })
       .catch(function (error) {
