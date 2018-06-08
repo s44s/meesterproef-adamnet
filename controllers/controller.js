0089b7fcabc9045bc8cc9d3773748192aa64385d
Max de Vries <max.devries.1995@gmail.com>
2018-06-08 14:04:35 +0200

    Remove fetch and add async loading of chapters

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 var fetch = require('node-fetch');
 var sparqlqueries = require('./sparql');
+var chapters = require('./chapters.js');
 
 exports.homePage = function (req, res, next) {
   res.render('index');
@@ -55,59 +56,13 @@ exports.postCreateStoryPage = function (req, res, next) {
   res.redirect('/');
 }
 
-exports.getCreateStoryPage = function (req, res, next) {
-  var rows = [];
+exports.getCreateStoryPage = async function (req, res, next) {
+  var result = await chapters.location(newStoryData);
+  console.log('result: ', result);
 
-  // Fetch the images for selected location and timestamp:
-  if (Object.keys(rows).length === 0) {
-    var url = sparqlqueries.url(sparqlqueries.getLocationAndTimestamp(newStoryData));
-
-    fetch(url)
-		.then((resp) => resp.json()) // transform the data into json
-      .then(function (data) {
-        console.log(data.results.bindings);
-
-        // First data filter:
-
-        /*
-        * Order the streets by distance to centerPoint (closest first)
-        * This gives us the order of the chapters based on location.
-        *
-        * Chapters based on location:
-        * ---------------------------
-        * 1. Closest street to centerPoint: probably your old/current street
-        * 2. Streets within a range of 25% from centerPoint: probably your neighbourhood
-        * 3. Other streets surrounding your neighbourhood
-        */
-
-        /*
-        * First chapter:
-        * --------------
-        * Filter the data for every image that has the following timestamp and location:
-        *
-        * Timestamp:
-        * ----------
-        * If timestamp <= 5: filter per year
-        * If timestamp > 5 & <= 20: filter per 2 years
-        * If timestamp > 20: filter per 5 years
-        *
-        * Location:
-        * ---------
-        * Take closest street to centerPoint as main location
-        */
-
-        res.render('create-story', {
-          dataFirstQuery: data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  } else {
-    res.render('create-story', {
-      dataFirstQuery: rows
-    });
-  }
+  res.render('create-story', {
+    dataFirstQuery: result
+  });
 }
 
 exports.saveStoryPage = function (req, res, next) {
