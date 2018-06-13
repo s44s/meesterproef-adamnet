922d92c2f457ae187198b17c1f7efcc78b77d088
Max de Vries <max.devries.1995@gmail.com>
2018-06-13 13:13:24 +0200

    Wrap fetch location and timestamp in own function

--- controllers/chapters.js
+++ controllers/chapters.js
@@ -2,71 +2,47 @@ var fetch = require('node-fetch');
 var sparqlqueries = require('./sparql');
 
 exports.location = async function (newStoryData) {
-  // Fetch the images for selected location and timestamp:
-  var url = sparqlqueries.url(sparqlqueries.getLocationAndTimestamp(newStoryData));
+  // Fetch the street Wkts:
 
-  return await fetch(url)
-	  .then((resp) => resp.json()) // transform the data into json
-    .then(function (data) {
+  var fetchLocationAndTimestamp = async function () {
+    // Fetch the images for selected location and timestamp:
+    var url = sparqlqueries.url(sparqlqueries.getLocationAndTimestamp(newStoryData));
 
-			var allData = data.results.bindings;
+    return await fetch(url)
+	    .then((resp) => resp.json()) // transform the data into json
+      .then(function (data) {
+			  return data.results.bindings;
+      }).catch(function (error) {
+        console.log(error);
+      });
+  }
 
-			var allDataMapped = {
-				years: {}
-			};
+  var allDataMapped = {
+    years: {}
+  };
 
-			allData.forEach(function(item, i, self) {
-		  	var year = item.start.value.split('-')[0];
-				var chapter;
+  var allData = await fetchLocationAndTimestamp();
 
-				if (item.street.value == allData[0].street.value) {
-					chapter = allData[0].streetLabel.value;
-				} else {
-					chapter = 'de overige straten';
-				}
+  allData.forEach(function(item, i, self) {
+    var year = item.start.value.split('-')[0];
+    var chapter;
 
-		    if (!allDataMapped.years[year]) {
-		    	allDataMapped.years[year] = {};
-		    }
-				if (!allDataMapped.years[year][chapter]) {
-					allDataMapped.years[year][chapter] = [];
-				}
+    if (item.street.value == allData[0].street.value) {
+      chapter = allData[0].streetLabel.value;
+    } else {
+      chapter = 'de overige straten';
+    }
 
-				allDataMapped.years[year][chapter].push(item);
-			});
+    if (!allDataMapped.years[year]) {
+      allDataMapped.years[year] = {};
+    }
+    if (!allDataMapped.years[year][chapter]) {
+      allDataMapped.years[year][chapter] = [];
+    }
 
-			return allDataMapped;
+    allDataMapped.years[year][chapter].push(item);
+  });
 
+  return allDataMapped;
 
-      // First data filter:
-
-      /*
-      * Order the streets by distance to centerPoint (closest first)
-      * This gives us the order of the chapters based on location.
-      *
-      * Chapters based on location:
-      * ---------------------------
-      * 1. Closest street to centerPoint: probably your old/current street
-      * 2. Streets within a range of 25% from centerPoint: probably your neighbourhood
-      * 3. Other streets surrounding your neighbourhood
-      */
-
-      /*
-      * First chapter:
-      * --------------
-      * Filter the data for every image that has the following timestamp and location:
-      *
-      * Timestamp:
-      * ----------
-      * If timestamp <= 5: filter per year
-      * If timestamp > 5 & <= 20: filter per 2 years
-      * If timestamp > 20: filter per 5 years
-      *
-      * Location:
-      * ---------
-      * Take closest street to centerPoint as main location
-      */
-    }).catch(function (error) {
-      console.log(error);
-    });
 };
