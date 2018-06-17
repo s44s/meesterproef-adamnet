5182336d52e392ade6cdd12d58ef05256f5eea2b
Max de Vries <max.devries.1995@gmail.com>
2018-06-17 13:05:31 +0200

    Add setup for searchbar

--- public/js/search-location.js
+++ public/js/search-location.js
@@ -2,4 +2,35 @@
 
   'use strict';
 
+  var searchLocation = {
+    currentFocus: 0,
+    init: function (searchbar) {
+      var self = this;
+
+      // Event listener for input value:
+      searchbar.addEventListener('input', function (e) {
+        self.closeAllLists();
+        if (!this.value) return false;
+        self.currentFocus = -1;
+        self.getAutocomplete(data, this.value);
+      });
+
+    },
+    getAutocomplete: function () {
+
+    },
+    setAutocomplete: function () {
+
+    },
+    addActive: function () {
+
+    },
+    removeActive: function () {
+
+    },
+    closeAllLists: function () {
+
+    }
+  };
+
 })();
